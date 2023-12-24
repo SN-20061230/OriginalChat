8bae12b2fac0238314fac90d273d285b04fc2f70
SN-20061230 <sardorbek2006.normurodov@gmail.com>
2023-12-24 13:29:37 +0500

    Hala Madrid

--- app/src/main/java/com/example/originalchat/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/originalchat/Screens/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -24,6 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.chat_app.ChatItem
@@ -34,7 +37,7 @@ import com.example.chat_app.Database.Main
 @Composable
 fun HomeScreen(navController: NavController) {
     var users by remember { mutableStateOf<List<String>>(emptyList()) }
-
+    val context = LocalContext.current
     Main.getUsers { list ->
         users = list
     }
@@ -51,7 +54,10 @@ fun HomeScreen(navController: NavController) {
                     contentDescription = "Settings Icon",
                     tint = Color(108, 120, 131),
                 )
+
             }
+            Text(Main.getSavedUser(context), color = Color.White, modifier = Modifier.padding(start = 150.dp, top = 10.dp))
+
         })
     }) { innerPadding ->
         LazyColumn(
